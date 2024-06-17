f618f44888aa23461581c8d4739965f705d27760
psharma <psharma8rrc.ca>
2024-06-17 08:44:08 -0500

    Final commit with all the updated code

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,4 @@
 class Product < ApplicationRecord
     belongs_to :category
-    validates :title, presence: true
-    validates :price, presence: true
-    validates :stock_quantity, presence: true
-end
+  end
+  
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,25 +1,4 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
-
-require 'faker'
-require "csv"
-
-# 676.times do
-#   Product.create(
-#     title: Faker::Commerce.product_name,
-#     description: Faker::Lorem.paragraph,
-#     price: Faker::Commerce.price,
-#     stock_quantity: Faker::Number.between(from: 1, to: 100)
-#   )
-# end
-require "csv"
+require 'csv'
 
 Product.destroy_all
 Category.destroy_all
@@ -29,18 +8,15 @@ csv_data = File.read(csv_file)
 
 products = CSV.parse(csv_data, headers: true)
 
-products.each do |row|
-  category_name = row['category_name']
+products.each do |product|
+  category_name = product['category']
   category = Category.find_or_create_by(name: category_name)
 
   Product.create(
-    title: row['title'],
-    description: row['description'],
-    price: row['price'],
-    stock_quantity: row['stock_quantity'],
+    title: product['title'],
+    description: product['description'],
+    price: product['price'],
+    stock_quantity: product['stock_quantity'],
     category: category
   )
 end
-
-
-
